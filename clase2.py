3156866cfd7d85f75f13e0cd24f8a5c8b7271fca
rxmcb1 <02240131026@mail.udes.edu.co>
2025-08-27 15:43:39 -0500

    Add files via upload

---
+++ clase2.py
@@ -0,0 +1,41 @@
+class saludo:
+    #metodo constructor
+    #se usa para inicializar obj
+    def __init__(self , dato_texto):
+        #atributo
+        self.mensaje = " xxxx "
+        print(self.mensaje)
+
+    def tomar_nombre(self):
+        nombre_usuario = input("escriba el nombre del usuario")
+        return nombre_usuario
+    
+    def crear_mensaje(self, dato_usuario):
+        self.mensaje = "hola usuario" + dato_usuario
+        imprimir_mensaje(self.mensaje)
+
+    def get_mensaje(self)
+    return self.mensaje
+
+    def set_atributo(self , dato_nuevo_mensaje):
+        self.mensaje =dato_nuevo_mensaje
+
+    def tomar_nombre(self , dato_usuario):
+        
+        
+
+
+
+# ***************** CODIGO PRINCIPAL **********************
+
+texto = "hola Usuario"
+
+obj_mensaje = saludo(texto)
+
+#obj_mensaje.tomar_nombre()
+
+
+    
+
+        
+
